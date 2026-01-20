2020b7b78d439ced67cb83d58e4d42a47abc780f
ReiShin-eva <bryanrahardja15@gmail.com>
2026-01-19 21:56:22 -0800

    Update StringUtilsTest.cpp

--- proj1/testsrc/StringUtilsTest.cpp
+++ proj1/testsrc/StringUtilsTest.cpp
@@ -2,7 +2,11 @@
 #include "StringUtils.h"
 
 TEST(StringUtilsTest, SliceTest){
-    
+    EXPECT_EQ(Slice("abcdef", 1, 4), "bcd");
+    EXPECT_EQ(Slice("abcdef", -3), "def"); 
+    EXPECT_EQ(Slice("abcdef", 2), "cdef"); 
+    EXPECT_EQ(Slice("abc", 5, 6), "");
+    EXPECT_EQ(Slice("", 0, 0), "");
 }
 
 TEST(StringUtilsTest, Capitalize){
